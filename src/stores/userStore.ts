8656e3119252912c3d340900fb0e0ec4b9f6950a
Francisco <sernaojuliofranciscojavier@gmail.com>
2025-04-20 21:47:50 -0500

    Modifiando useUser que el rol del usuario registrado como estudiante está haciendo acciones como obtener usuarios registrados, permiso de un admin

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,22 +1,16 @@
 import { create } from "zustand";
 import type { User, UserRegister } from "@/types/User";
-import { updateProfile, getAllUsers } from "@/services/userService";
+import { updateProfile } from "@/services/userService";
 
 interface UserState {
   users: User[];
   updateProfile: (userData: UserRegister) => Promise<string | undefined>;
-  fetchGetUsers: () => Promise<void>;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const useUserStore = create<UserState>((set) => ({
   users: [],
   updateProfile: async (userData) => {
     return await updateProfile(userData);
-  },
-  fetchGetUsers: async () => {
-    const users = await getAllUsers();
-    if (users) {
-      set({ users });
-    }
   },
 }));
